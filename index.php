d43fbbfda0856b0cf072e77904b53eb958a1f1dd
Chad Drennan <cdrennan@mail.greenriver.edu>
2020-02-03 15:51:38 -0500

    sent data to results page

--- index.php
+++ index.php
@@ -10,7 +10,7 @@ require "vendor/autoload.php";
 require_once ('model/validation-functions.php');
 $f3 = Base::instance();
 $f3->set('DEBUG', 3);
-$f3->set('colors', array('pink', 'green', 'blue'));
+$f3->set('colors', array('pink', 'black', 'brown', 'white'));
 
 
 $f3->route('GET /',function ()
@@ -49,12 +49,12 @@ $f3->route('GET /@item', function($f3, $params) {
 
 $f3->route('GET|POST /order',function ($f3){
     $_SESSION = [];
-    session_destroy();
+
     if(isset($_POST['animal'])) {
         $animal = $_POST['animal'];
-        if(validText($animal)) {
+        if(validString($animal)) {
             $_SESSION['animal'] = $animal;
-            $f3->reroute('/order2.html');
+            $f3->reroute('/order2');
         }
         else {
             $f3->set("errors['animal']", "Please enter an animal");
@@ -65,20 +65,29 @@ $f3->route('GET|POST /order',function ($f3){
     //use it to render the main page
 });
 
-$f3->route('GET|POST /order2',function ()
+$f3->route('GET|POST /order2',function ($f3)
 {
-    $_SESSION['animal'] = $_POST['animal'];
+    if(isset($_POST['color'])) {
+        $color = $_POST['color'];
+        if(validColor($color)) {
+            $_SESSION['color'] = $color;
+            $view = new Template();//template object
+            echo $view-> render('views/results.html');
+            return;
+        }
+        else {
+            $f3->set("errors['color']", "Please enter a color");
+        }
+    }
     $view = new Template();//template object
     echo $view-> render('views/form2.html');//use it to render the main page
 
 });
 
-$f3->route('POST /results',function ()
-{
-    $_SESSION['color'] = $_POST['color'];
-    $view = new Template();//template object
-    echo $view-> render('views/results.html');//use it to render the main page
-
-});
+//$f3->route('POST /results',function ()
+//{
+//
+//
+//});
 
 $f3->run();
